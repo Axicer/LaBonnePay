b4602495bd65dac67281369608ffa4781e3370b6
Julien <Julien@Julien-PC>
2015-07-19 17:00:27 +0200

    UPDATE

---
+++ LaBonnePay/src/fr/axicer/LBP/CommandListener.java
@@ -0,0 +1,20 @@
+package fr.axicer.LBP;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class CommandListener implements Listener{
+	
+	Main pl;
+	
+	public CommandListener(Main pl) {
+		this.pl = pl;
+	}
+	
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void CommandPreProcess(PlayerCommandPreprocessEvent ev){
+		
+	}
+}
--- LaBonnePay/src/fr/axicer/LBP/Main.java
+++ LaBonnePay/src/fr/axicer/LBP/Main.java
@@ -1,5 +1,16 @@
 package fr.axicer.LBP;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(new CommandListener(this), this);
+		getLogger().info("Plugin ON");
+		getLogger().info("Plugin by Axicer_");
+	}
+	@Override
+	public void onDisable() {
+		getLogger().info("Plugin OFF");
+	}
 }
