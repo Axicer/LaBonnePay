a564a3842b3d6f3fa37aae8cb149481f7bc36c6d
Julien <Julien@Julien-PC>
2015-07-19 17:26:37 +0200

    FINAL BEFORE TEST

--- LaBonnePay/src/fr/axicer/LBP/CommandListener.java
+++ LaBonnePay/src/fr/axicer/LBP/CommandListener.java
@@ -1,5 +1,7 @@
 package fr.axicer.LBP;
 
+import net.md_5.bungee.api.ChatColor;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -8,15 +10,18 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class CommandListener implements Listener{
 	
-	Main pl;
-	
-	public CommandListener(Main pl) {
-		this.pl = pl;
-	}
-	
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void CommandPreProcess(PlayerCommandPreprocessEvent ev){
 		Player p = ev.getPlayer();
-		
+		if(Main.getCommandPrice().containsKey(ev.getMessage())){
+			if(Vault.getEconomy().getBalance(p) >= Main.getCommandPrice().get(ev.getMessage())){
+				Vault.getEconomy().withdrawPlayer(p, Main.getCommandPrice().get(ev.getMessage()));
+				p.sendMessage(ChatColor.GREEN+"-"+Main.getCommandPrice().get(ev.getMessage()));
+			}else{
+				p.sendMessage(ChatColor.RED+"Tu n'as pas assez d'argent !");
+				p.sendMessage("La commande coute: "+Main.getCommandPrice().get(ev.getMessage())+"$");
+				ev.setCancelled(true);
+			}
+		}
 	}
 }
